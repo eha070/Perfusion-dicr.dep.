b878e20ec7d5f9821a3026ac235698e0fbab743a
Erlend Hodneland <erlend.hodneland@uib.no>
2015-02-24 12:34:34 +0100

    Adding settings.m in root folder

--- settings.m
+++ settings.m
@@ -1,7 +1,15 @@
-function [prm,Qmat] = settings
+function [prm,Fmat] = settings
+
+prm.reconflowIO = 1e-1;
+prm.reconflowalpha = 0;
+prm.reconflowregularization = '';
+prm.reconflowroitype = 'full';
+prm.reconflowcode = 'classic';
+prm.reconflowdata = 'synt';
+prm.reconflowdeconvmethod = 'circSVD';
 
 % reduce time steps
-prm.stepred = 20;
+prm.stepred = 100;
 
 % type of porosity
 prm.phiopt = 'flat';
@@ -13,12 +21,14 @@ prm.aiftype = 'gamma';
 % prm.aiftype = 'parker';
 % prm.aiftype = 'delta';
 
-prm.physdim = [10,10,0.1];
+prm.physdim = [10,10,1];
+prm.physvol = prod(prm.physdim);
 
 % dimension of problem
 %prm.dim = [32,32,1];
-% prm.dim = [64,64,1];
-prm.dim = [128,128,1];
+prm.dim = [64,64,1];
+% prm.dim = [20,20,1];
+% prm.dim = [128,128,1];
 % prm.dim = [256,256,1];
 % prm.dim = [512,512,1];
 
@@ -27,7 +37,6 @@ dim = prm.dim;
 
 % voxel size in mm
 prm.h = prm.physdim./prm.dim;
-%prm.h = [1,1,1]*0.1;
 
 % voxel volume in ml
 prm.voxelvol = prod(prm.h);
@@ -36,10 +45,10 @@ prm.voxelvol = prod(prm.h);
 prm.mu = 5*1e-6;
 
 % timeline and dt
-% prm.T = 240;
-prm.T = 480;
+prm.T = 90;
+% prm.T = 480;
 % prm.T = 40;
-prm.dt = 0.01;
+prm.dt = 0.002;
 % prm.aifpeak = 7;
 % prm.aifwidth = 0.3;
 prm.timeline = (0:prm.dt:prm.T);
@@ -48,8 +57,13 @@ prm.timeline = (0:prm.dt:prm.T);
 %
 % Source and sink
 %
+a = zeros(dim);
+a(1,1) = 1;
+ind = find(a);
+[cso(:,1),cso(:,2),cso(:,3)] = ind2sub(dim,ind);
+prm.cso = cso;
 
-prm.cso = [1,1,1];
+% prm.cso = [1,1,1];
 % prm.cso = [1,1,1;...
 %     2,1,1;...
 %     1,2,1;...
@@ -59,8 +73,14 @@ prm.cso = [1,1,1];
 % prm.cso(:,3) = 1;
 prm.nso = size(prm.cso,1);
 
+a = zeros(dim);
+a(end,end) = 1;
+ind = find(a);
+[csi(:,1),csi(:,2),csi(:,3)] = ind2sub(dim,ind);
+prm.csi = csi;
+
 %prm.csi = [dim(1),1,1];
-prm.csi = [dim(1),dim(2),1];
+% prm.csi = [dim(1),dim(2),1];
 % prm.csi = [dim(1),1,1;...
 %     dim(1)-1,1,1;...
 %     dim(1),2,1;...
@@ -71,46 +91,41 @@ prm.csi = [dim(1),dim(2),1];
 prm.nsi = size(prm.csi,1);
 
 % Perfusion of 5-70ml/min/100ml (From constantin)
-% Go for a perfusion of 50ml/min/100ml
-% Comment: 50/60/100 is 0.0083ml/s/ml = 0.0083mm^3/s/mm^3 as average flow in the
-% brain
-% qin = 50;
-% 
-% % scale to ml/sec/100ml = mm^3/s/100mm^3
-% qin = qin/60;
-% 
-% % scale to mm^3/s/mm^3
-% qin = qin/100;
+% Go for a perfusion of 50ml/min/100ml 
+qin = 50;
+
+% This corresponds to 0.5ml/min/ml = 0.5mm^3/min/mm^3
+qin = qin/100;
+
+% Multiply by volume of our slab to get the total flow into the region to
+% get ml/min
+qin = qin*prm.physvol;
+
+% Divide by 60 to get per second: mm^3/sec
+qin = qin/60;
 
 % this value gives a perfusion of around 50ml/min/100ml!!! Thats why we use
 % it.
-qin = 0.0007;
+% qin = 0.0007;
 
 % Actually, must multiply Q with a unit converter a = 1(ml/mm^3), becoming
 % Q*a/rho
-Qmat = zeros(dim);
-
-% % volume of THIS brain in mm^3: 
-% brainvol = prod(dim)*prm.voxelvol;
-% 
-% % scale the flow to feed the brain in total (mm^3/s)
-% qin = qin*brainvol;
-
-% scaling it to the voxel volume to have a flow mm^3/s/mm^3 in the source
-% shouldn't this be here????
-% qin = qin/prm.voxelvol; 
-
+Fmat = zeros(dim);
 
 qout = -qin;
 for i = 1 : prm.nso
-    Qmat(prm.cso(i,1),prm.cso(i,2),prm.cso(i,3)) = qin;
-    Qmat(prm.csi(i,1),prm.csi(i,2),prm.csi(i,3)) = qout;
+    Fmat(prm.cso(i,1),prm.cso(i,2),prm.cso(i,3)) = qin/prm.nso;
+    Fmat(prm.csi(i,1),prm.csi(i,2),prm.csi(i,3)) = qout/prm.nsi;
 end;
 
 % fluid density map in units mg/mm^3
 prm.rho = 1;
 
 % scale away the density
-Qmat = Qmat/prm.rho;
+Fmat = Fmat/prm.rho;
+
+% for plotting Cmat
+prm.scaling = 1;
+
 
 
