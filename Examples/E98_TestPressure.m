a1bebf8eade28cb03b6fb9348d0d9bdf312fe3fd
Erlend Hodneland <erlend.hodneland@uib.no>
2015-04-07 15:18:01 +0200

    Merging changes of SI units

--- Examples/E98_TestPressure.m
+++ Examples/E98_TestPressure.m
@@ -19,54 +19,47 @@ clc
 [prm,Fmat]   = settings;
 basenameflow = perfusion1c.providenameflow(prm.phiopt,prm.Kopt,prm.dim);
 pathload = ['results/synt-createflowTPFA-' basenameflow '.mat'];
-msg = ['Loading ' pathload];
-disp(msg);
 D = load(pathload);
 
 %get pressure
-u     = D.pmat;
+u = D.pmat;
 
 %get physiological dimensions
-m     = size(u);
-h     = prm.physdim(1:2)./m;
+m = size(u);
+h = prm.h;
 
-%setup permeabiltiy K and viscosity mu
 K = D.Kmat(1,1,1,1);
 mu = prm.mu;
-
-%K  = 5e-6;
-%mu = 5e-6; %i don't find mu in the code, the paper says its 5e-6
 
 %% build laplacian
 
 %function handle for identity
 id = @(i) speye(m(i));
 
-%get finite difference matrix for first derivative with neumann boundary
-%conditions
-D           = @(i) spdiags(ones(m(1)+1,1)*[-1,1],[-1,0],m(1)+1,m(1));
+%setup finite difference matrix for laplacian in 1D
+Li = @(i) spdiags(ones(m(1)+1,1)*[1,-2,1],[-1,0,1],m(1),m(1)); 
 
 
-D1          = D(1)./h(1);
-D1(1,1)     = 0;
-D1(end,end) = 0;
+%get 1D-derivative-matrices with neumann boundary conditions
+L1          = Li(1);
+L1(1,1)     = -1;
+L1(end,end) = -1;
+L1          = L1./h(1)^2;
 
-D2          = D(2)./h(2);
-D2(1,1)     = 0;
-D2(end,end) = 0;
+L2          = Li(2);
+L2(1,1)     = -1;
+L2(end,end) = -1;
+L2          = L2./h(2)^2;
 
-%get discrete gradient
-Di   = kron(id(2),D1);
-Dj   = kron(D2,id(1));
-GRAD = [Di;Dj];
-DIV  = GRAD';
-LAP  = DIV*GRAD;
+%kronecker to get the full Laplacian
+LAP = kron(id(2),L1) + kron(L2,id(1));
+
 
 
 %% calculate -K/mu*\Delta u and visualize results
-f = K/mu*(LAP*u(:));
+f = -K/mu*(LAP*u(:));
 f = reshape(f,m);
-% Convert f (which we call Q in our paper) from m^3/s/m^3 to m^3/s which are the units of Fmat 
+% Convert from m^3/s/m^3 to m^3/s which are the units of Fmat
 f = f*prod(prm.h);
 
 figure(1); clf;
@@ -84,4 +77,3 @@ subplot(1,3,3);
 imagesc(f - Fmat);
 axis image;
 title('f - Fmat')
-
