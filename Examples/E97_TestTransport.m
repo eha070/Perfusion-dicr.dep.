141864e26abf41ceb44c6ba00681abd2bfc3ae22
Constantin Heck <constantin.heck@mic.uni-luebeck.de>
2016-02-21 18:42:55 +0100

    Extended E97_TestTransport, however this is still not finished

--- Examples/E97_TestTransport.m
+++ Examples/E97_TestTransport.m
@@ -21,137 +21,64 @@ clc
 basenameflow = perfusion1c.providenameflow(prm.phiopt,prm.Kopt,prm.dim);
 pathload = ['results/synt-createflowTPFA-' basenameflow '.mat'];
 D = load(pathload);
-pathload = './results/synt-createindicatorpde-phi-flat-K-flat-dim-64-aif-delta-T-90-red-25.mat';
+pathload = './results/synt-createindicatorpde-phi-flat-K-flat-dim-64-aif-gamma-T-90-red-25.mat';
 E = load(pathload);
 
 %get physiological dimensions
 m        = prm.dim(1:2);
 h        = prm.h;
 voxelvol = prod(prm.h);
+dim      = numel(h);
 
 %get Cmat, qmat and phimat
-C   = E.Cmat;
-AIF = reshape(E.aifval,[],1);
+C    = E.Cmat;
+AIF  = reshape(E.aifval,[],1);
+qmat = D.qmat;
+phi  = D.phimat;
+k    = numel(AIF);
+
+%% do the testing: 
+% We know that dC/dt = div(q*c) + FIn*AIF - FOut*c
+% reformulation: dC/dt = div(q)*c + q*GRADc + FIn*AIF - FOut*c
+% We will calculate:
+% dCdt = divqc + qGRADc + FInAIF - FOutc
+%
+%REMARK: This is probably not the best idea: Maybe I should just sum up
+%differences over the faces
 
 
+%setup gradient operator with Neumann-bc
+e  =@(i) ones(m(i),1);
+id =@(i) speye(m(i));
 
+D1 = spdiags(e(1)*[-1,1],[-1,0],m(1)+1,m(1));
+D1(1,1) = 0; D1(end,end)=0; D1(end,end-1)=0;
 
-%% build laplacian
+D2 = spdiags(e(2)*[-1,1],[-1,0],m(2)+1,m(2));
+D2(1,1) = 0; D2(end,end)=0; D2(end,end-1)=0;
 
-%function handle for identity
-id = @(i) speye(m(i));
+%get normalized flow q and normalized source-field fmat
+si   =@(i) prod(h)/h(i);
+q{1} = qmat{1}./si(1);
+q{2} = qmat{2}./si(2);
+q{3} = qmat{3}./si(3);
+fmat = Fmat./hd;
 
-%setup finite difference matrix for laplacian in 1D
-Li = @(i) spdiags(ones(m(i)+1,1)*[1,-2,1],[-1,0,1],m(i),m(i)); 
+%setup cmat
+cmat = bsxfun(@times,Cmat,phimat);
 
-%get 1D-derivative-matrices with neumann boundary conditions
-L1          = Li(1);
-L1(1,1)     = -1;
-L1(end,end) = -1;
-L1          = L1./h(1)^2;
+%setup divqc (rember that div(q)=fmat
+divqc = bsxfun(@times,cmat,fmat);
 
-L2          = Li(2);
-L2(1,1)     = -1;
-L2(end,end) = -1;
-L2          = L2./h(2)^2;
+%setup qGRADc
+cTmp  = reshape(cmat,[],k);
 
-%kronecker to get the full Laplacian
-LAP = kron(id(2),L1) + kron(L2,id(1));
+m1  = size(q{1});
+m2  = size(q{2});
+D1c = reshape(D1*cTmp,[m1,k]);
+D2c = reshape(D2*cTmp,[m2,k]);
 
+%multiply GRADc and q
+qGRADc1 = bsxfun(@times,D1c,q{1});
+qGRADc2 = bsxfun(@times,D2c,q{2});
 
-%% get Gradient to setup the flow
-
-%setup finite difference matrix for laplacian in 1D
-Di = @(i) spdiags(ones(m(1)+1,1)*[-1,1],[-1,0],m(i)+1,m(i));
-
-
-%get 1D-derivative-matrices with neumann boundary conditions
-D1          = Di(1);
-D1(1,1)     = 0;
-D1(end,end) = 0;
-D1          = D1./h(1);
-
-D2          = Di(2);
-D2(1,1)     = 0;
-D2(end,end) = 0;
-D2          = D2./h(2);
-
-%kronecker to get the full Gradient
-GRADy = kron(id(2),D1);
-GRADx = kron(D2,id(1));
-
-
-
-%% calculate -K/mu*\Delta u and visualize results
-
-f = -K/mu*(LAP*p);
-f = reshape(f,m);
-
-% Convert from m^3/s/m^3 to m^3/s which are the units of Fmat
-f = f*voxelvol;
-
-%get surface for normalization. In our case it is isotropic
-Ay = h(2)*h(3);
-Ax = h(1)*h(3);
-
-%compare flow as well
-qTest{1} = reshape(GRADy*p*Ay,m(1)+1,m(2))*(-K/mu);
-qTest{2} = reshape(GRADx*p*Ax,m(1),m(2)+1)*(-K/mu);
-
-
-
-
-return;
-%% results
-
-figure(1); clf;
-set(1,'name','Pressure');
-subplot(1,3,1);
-imagesc(f);
-axis image;
-title('f')
-
-subplot(1,3,2);
-imagesc(Fmat);
-axis image;
-title('Fmat')
-
-subplot(1,3,3);
-imagesc((f - Fmat)./f);
-axis image;
-title('(f - Fmat)./f')
-
-
-figure(2);clf;
-set(2,'name','flow');
-
-subplot(2,3,1);
-imagesc(q{1});
-axis image;
-title('q{1}')
-
-subplot(2,3,2);
-imagesc(qTest{1});
-axis image;
-title('qTest{1}')
-
-subplot(2,3,3);
-imagesc((qTest{1} - q{1})./qTest{1});
-axis image;
-title('(qTest{1} - q{1})./qTest{1}')
-
-
-subplot(2,3,4);
-imagesc(q{2});
-axis image;
-title('q{2}')
-
-subplot(2,3,5);
-imagesc(qTest{2});
-axis image;
-title('qTest{2}')
-
-subplot(2,3,6);
-imagesc((qTest{2} - q{2})./qTest{2});
-axis image;
-title('(qTest{2} - q{2})./qTest{2}')
